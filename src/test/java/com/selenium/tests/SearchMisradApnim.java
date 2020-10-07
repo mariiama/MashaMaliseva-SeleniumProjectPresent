02b81d3203c46c0b9350981fa10f5d52ce143307
mariiama <mariiamalisheva@gmail.com>
2020-10-07 17:18:36 +0300

    first commit

---
+++ src/test/java/com/selenium/tests/SearchMisradApnim.java
@@ -0,0 +1,38 @@
+package com.selenium.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class SearchMisradApnim {
+    WebDriver wd;
+    @BeforeMethod
+    public void setUp(){
+        wd = new ChromeDriver();
+        wd.manage().window().maximize();
+        wd.manage().timeouts().implicitlyWait (20, TimeUnit.SECONDS);
+        wd.navigate().to("https://www.gov.il/");
+
+    }
+    @Test
+    public void searchFromNavMenuTest(){
+        //click on Kablat Kahal
+        wd.findElement(By.cssSelector("#tm-4")).click();
+        // get page titel
+        String pageTitle = wd.findElement(By.cssSelector("h1")).getText();
+        System.out.println(pageTitle);
+
+
+
+    }
+    @AfterMethod (enabled = false)
+    public void tearDown(){
+        wd.quit();
+
+    }
+}
